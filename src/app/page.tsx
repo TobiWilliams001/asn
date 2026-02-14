94590854f9c90ff672a0025beb79381937d52926
TobiWilliams003 <tobiwilliams003@gmail.com>
2026-02-14 11:54:14 +0100

    feat(pages): assemble updated homepage with new component structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
-import Image from 'next/image'
-import About from '../components/About/About'
-import Hero from '../components/Hero/Hero'
+import Hero from '@/components/Hero/Hero';
+import WhoWeAre from '@/components/WhoWeAre/WhoWeAre';
+import Mission from '@/components/Mission/Mission';
+import ValueProposition from '@/components/ValueProposition/ValueProposition';
+import OurReach from '@/components/OurReach/OurReach';
+import GetInvolved from '@/components/GetInvolved/GetInvolved';
+import BlogSectionHome from '@/components/BlogSectionHome/BlogSectionHome';
+import YouTubeSectionHome from '@/components/YouTubeSectionHome/YouTubeSectionHome';
+import Donation from '@/components/Donation/Donation';
+import Footer from '@/components/Footer/Footer';
 
 export default function Home() {
   return (
     <main className="min-h-screen w-full">
       <Hero />
-      <About />
+      <WhoWeAre />
+      <Mission />
+      <ValueProposition />
+      <OurReach />
+      <GetInvolved />
+      <Donation />
+      <BlogSectionHome />
+      <YouTubeSectionHome />
+      <Footer />
     </main>
-  )
+  );
 }
-
