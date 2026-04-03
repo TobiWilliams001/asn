4562b19d284ac7a8df4e642057755c2e0f8c7db9
TobiWilliams003 <tobiwilliams003@gmail.com>
2026-04-03 13:40:09 +0100

    feat: add Firebase Storage support to learnConfig

--- src/firebase/learnConfig.ts
+++ src/firebase/learnConfig.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const learnFirebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_LEARN_FIREBASE_API_KEY,
@@ -22,4 +23,5 @@ try {
 
 export const learnAuth = getAuth(learn_app);
 export const learnDb = getFirestore(learn_app, 'default');
+export const storage = getStorage(learn_app);
 export default learn_app;
